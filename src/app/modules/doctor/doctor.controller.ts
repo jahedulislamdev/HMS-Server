a7cfe4320a30372d8901b51dc9c80fa0440c033c
jishan <jahedulislamdev@gmail.com>
2026-08-22 14:35:39 +0600

    add: integrate querybuilder to doctor service

--- src/app/modules/doctor/doctor.controller.ts
+++ src/app/modules/doctor/doctor.controller.ts
@@ -4,10 +4,12 @@ import { doctorService } from "./doctor.service";
 import sendResponse from "../../shared/sendResponse";
 import { StatusCodes } from "http-status-codes";
 import { UserRole } from "../../../generated/prisma/enums";
+import { IQueryParams } from "../../interface/query.Interface";
 
 //* get all doctor
 const getDoctors = catchAsync(async (req: Request, res: Response) => {
-    const result = await doctorService.getDoctors();
+    const query = req.query;
+    const result = await doctorService.getDoctors(query as IQueryParams);
     sendResponse(res, {
         statusCode: StatusCodes.OK,
         message: "doctors retrieved successfully",
